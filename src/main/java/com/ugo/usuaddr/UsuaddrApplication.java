53432e0e6ef7bb3637250e635bb381d3dae44098
Ugo César <ugocesar19@hotmail.com>
2025-05-27 22:18:49 -0300

    Initial commit with the fresh created project. Made with Spring Initializr.

---
+++ src/main/java/com/ugo/usuaddr/UsuaddrApplication.java
@@ -0,0 +1,13 @@
+package com.ugo.usuaddr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuaddrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuaddrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ugo/usuaddr/UsuaddrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugo.usuaddr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuaddrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
